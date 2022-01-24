828a25d72c4f0aa07f9f9eeb8eab8c536e8bb74c
Ciro132000 <c.g_ciro@hotmail.com>
2022-01-24 12:18:10 -0500

    primero

--- src/components/course/Prueba.vue
+++ src/components/course/Prueba.vue
@@ -1,10 +1,21 @@
 <template>
-    <div>
-        Soy cursos!!!
-    </div>
+    <b-container>
+        <div class="row py-5">
+            <div class="col-md-8">
+                gfg
+            </div>
+            <div class="col-md-4 bg-white rounded flex-column">
+                
+            </div>
+        </div>
+    </b-container>
 </template>
 <script>
 export default {
     name: 'Prueba'
 }
 </script>
+
+<style scoped>
+
+</style>
