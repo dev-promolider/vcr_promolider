45e1c53d33e4ce346e42b62e1414fb0d88025fb7
Gianpier <Gianpiervicentemelgarejo@gmail.com>
2022-01-28 01:02:06 -0500

    gianpier-cambios-cursos

--- src/components/courses/Courses.vue
+++ src/components/courses/Courses.vue
@@ -1,37 +1,17 @@
 <template>
   <div class="container-courses">
     <div class="spinners" v-if="lord">
-      <b-spinner
-        variant="success"
-        type="grow"
-        label="Spinning"
-        class="mr-4"
-      ></b-spinner>
-      <b-spinner
-        variant="success"
-        type="grow"
-        label="Spinning"
-        class="mr-4"
-      ></b-spinner>
-      <b-spinner
-        variant="success"
-        type="grow"
-        label="Spinning"
-        class="mr-4"
-      ></b-spinner>
-      <b-spinner
-        variant="success"
-        type="grow"
-        label="Spinning"
-        class="mr-4"
-      ></b-spinner>
+      <div class="loader">Loading...</div>
     </div>   
     <div class="buscador" v-if="guardar">
       <input type="text" placeholder="buscar el curso requerido" />
       <img src="@/assets/logo.png" alt="" />
     </div>
 
-<div class="caja">
+<Carrousel :movies="movies" v-if="guardar"/>
+<CarrouselCourses :courses ="courses" v-if="guardar" />
+
+<!-- <div class="caja">
 
   <button class="boton1">A</button>
   <div class="cajita" v-for="items in informacion" :key="items.id">
@@ -40,21 +20,24 @@
 
   </div>
 <button class="boton2" v-on:click="aumentar()">A</button>
-</div>
+</div> -->
 
-
-    
-
+<!-- https://www.youtube.com/watch?v=JbXLkhIfSTQ -->
   
   </div>
 </template>
 
 <script>
 // import Spinner from '@/components/auth/Spinner.vue'
+import Carrousel from '@/components/courses/Carrousel-preferences.vue';
+import CarrouselCourses from '@/components/courses/Carrousel-cursos.vue'
 export default {
   name: "Prueba",
   components: {
     // Spinner
+    Carrousel,
+    CarrouselCourses
+
   },
   data() {
     return {
@@ -68,7 +51,29 @@ export default {
       noexis: false,
       loading: true,
       mostrar:false,
-      // age: 12345
+
+      title:'1231',
+      age: 123,
+      courses:[
+       
+      ],
+      movies:[
+        {id:1, banner: 'https://picsum.photos/600/300/?image=25' },
+        {id:2, banner: 'https://picsum.photos/600/300/?image=25' },
+        {id:3, banner: 'https://picsum.photos/600/300/?image=25' },
+
+        {id:4, banner: 'https://picsum.photos/600/300/?image=25' },
+        {id:5, banner: 'https://picsum.photos/600/300/?image=25' },
+        {id:6, banner: 'https://picsum.photos/600/300/?image=25' },
+
+        {id:7, banner: 'https://picsum.photos/600/300/?image=25' },
+        {id:8, banner: 'https://picsum.photos/600/300/?image=25' },
+        {id:9, banner: 'https://picsum.photos/600/300/?image=25' },
+
+        {id:10, banner: 'https://picsum.photos/600/300/?image=25' },
+        {id:11, banner: 'https://picsum.photos/600/300/?image=25' },
+        {id:12, banner: 'https://picsum.photos/600/300/?image=25' },
+      ]
       
     };
   },
@@ -80,37 +85,45 @@ export default {
          this.loading= false;
           this.mostrar=true;
         const array = datos.data.data;
+        // const array2 =datos.data.data;
         // this.informacion = array[0].courses_related;
-        console.log(array);
-       
+        console.log(array[0].courses_related)
+      //  console.log(array2[1].last_courses)
       //  this.informacion.forEach(cursos =>{
          
       //  })
-        this.total = array[0].courses_related;
+         this.courses = array[0].courses_related;
+      
+
+         console.log(this.courses)
         
-        for (let i = this.informacion.length; i < this.limite; i++) {
-          this.informacion.push(this.total[i]);
-          this.lorde = false;
+        // for (let i = this.informacion.length; i < this.limite; i++) {
+        //   this.informacion.push(this.total[i]);
+        //   this.lorde = false;
           //console.log("funciona")
-        }
+      //   }
       });
+    
     },
-    aumentar() {
-      this.limite += 5;
-      this.lorde = true;
-      if (this.informacion.length == this.total.length - 1) {
-        this.prendido = false;
-        this.lorde = false;
-        this.noexis = true;
+    // aumentar() {
+    //   this.limite += 5;
+    //   this.lorde = true;
+    //   if (this.informacion.length == this.total.length - 1) {
+    //     this.prendido = false;
+    //     this.lorde = false;
+    //     this.noexis = true;
          
-      }
+    //   }
       // this.informacion.forEach(cursos=>{
       //   this.age =cursos.id + 1
       //   console.log(this.age)
       // })
-      this.getAttributes();
-    },
- 
+      // this.getAttributes();
+    // },
+
+    somar(){
+      return this.age + this.title
+          }
   
   },
   created() {
@@ -141,7 +154,7 @@ export default {
   left: 0;
 }
 .buscador {
-  align-self: flex-end;
+ 
   background: rgb(255, 255, 255);
   width: 280px;
   height: 38px;
@@ -175,7 +188,7 @@ export default {
 .caja{
   width: 100%;
   max-width: 100%;
-  overflow: auto;
+  overflow:auto;
   margin-right: auto;
   margin-left: auto;
   height: 70%;
@@ -184,6 +197,9 @@ export default {
   position: relative;
   
 }
+/* .caja::-webkit-scrollbar{
+  width:0;
+} */
 .cajita{
   width: 600px;
   height: 158px;
@@ -197,9 +213,52 @@ export default {
   
 }
 .boton2{
-  position: relative;
+  position: absolute;
   right: 0;
   margin-top: 40px;
 }
 /* terimna aqui!! */
+
+.loader,
+.loader:after {
+  border-radius: 50%;
+  width: 10em;
+  height: 10em;
+}
+.loader {
+  margin: 60px auto;
+  font-size: 10px;
+  position: relative;
+  text-indent: -9999em;
+  border-top: 1.1em solid rgba(167, 133, 133, 0.2);
+  border-right: 1.1em solid rgba(167, 133, 133, 0.2);
+  border-bottom: 1.1em solid rgba(167, 133, 133, 0.2);
+  border-left: 1.1em solid #32a141;
+  -webkit-transform: translateZ(0);
+  -ms-transform: translateZ(0);
+  transform: translateZ(0);
+  -webkit-animation: load8 1.1s infinite linear;
+  animation: load8 1.1s infinite linear;
+}
+@-webkit-keyframes load8 {
+  0% {
+    -webkit-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+  100% {
+    -webkit-transform: rotate(360deg);
+    transform: rotate(360deg);
+  }
+}
+@keyframes load8 {
+  0% {
+    -webkit-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+  100% {
+    -webkit-transform: rotate(360deg);
+    transform: rotate(360deg);
+  }
+}
+
 </style>
