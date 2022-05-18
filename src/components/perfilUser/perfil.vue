678cd2275e5e6cdc0ed0b372aa2d1471c148702c
kenyichavez <chavezkenyi2@gmail.com>
2022-05-17 23:30:44 -0500

    imagen perfil

--- src/components/perfilUser/perfil.vue
+++ src/components/perfilUser/perfil.vue
@@ -1,85 +1,120 @@
 <template>
-  <div class="contenedor text-capitalize">
-    <div class="cuadro1 bg-white shadow bg-body rounded " >
-     
-      <div class="parrafos ">
-        <div class="img1 ">
-          <img src="../../assets/logo-perfil.png" alt=""  class="imagen1 "/>
-        </div>
-        <div class="p1">
-          <p class="text-dark ">{{ userUp.name }}</p>
-        </div>
-        <div class="p2">
-          <p class="text-dark">{{ userUp.email }}</p>
-        </div>
-      </div>
-      <div class="perfil">
-        <p class="text-dark "><strong> Rol de usuario:</strong> {{ userUp.rol }}</p>
-        <p class="text-dark "><strong>Plan actual:</strong> {{ userUp.plan }}</p>
-        <div class="pais">
-          <p class="text-dark text-center">Lima,Peru
-            <img class="icoban" src="../../assets/logo-bandera.svg" />
-          </p>
-        </div>
-      </div>
+  <div class="contenedor container text-capitalize">
+    <div class="row text-center">
 
-    </div>
+      <div class="cuadro1 col-md-5 text-center">
 
-    <div class="cuadro2 bg-white shadow p-3 mb-5 bg-body rounded text-center">
-      <form class="formulario1-2  " action="">
-        <p class=" parrafo text-dark my-5" >cambiar contraseña</p>
-        <!-- style="font-family: Arial black" -->
-        <input
-          class="contraseña my-4 col-10 bg-light border border-white"
-          type="text"
-          required
-          placeholder="contraseña"
-        />
+          <div class="row m-2 bg-white shadow bg-body rounded text-center w-100">
 
-        <input
-          class="contraseña my-4 col-10 bg-light border border-white"
-          type="text"
-          required
-          placeholder="repetir contraseña"
-        />
 
-        <div class="botonguardar1">
-          <input
-            class="btn-custom col-5 mt-5"
-            type="submit"
-            value="Guardar"
-            name=""
-            id=""
-          />
-        </div>
-      </form>
-    </div>
+            <div class="d-flex flex-column justify-content-center align-items-center parrafos my-3 w-100">
 
-    <div class="cuadro3 bg-white shadow p-3 mb-5 bg-body rounded text-center">
 
-      <div class="cuadro2-1 mt-5">
+                <img :src="this.picture" alt="" class="imagen img-fluid rounded-start mt-2" style="width: 150px; height:150px;"/>
+                <label for="">Editar <img class="lapiz" src="../../assets/logo-lapiz.svg" alt="" @click="editarImg"/></label>
+                
 
-        <div class="img2">
-          <img src="../../assets/logo-perfil.png" alt="" class="imagen2" />
-          <div>
-            <img class="lapiz m-2" src="../../assets/logo-lapiz.svg" alt="" />
-          </div>
-        </div>
+                  <div v-if="mostrar" class="editImg w-100">
 
-        
-          <div class="nombre mt-5">
+                    <div class="d-flex flex-column p-2 w-100">
+                      <div class="m-2 w-100">
+                        <img :src='this.picture' class="img-edit img-fluid rounded-start my-3" alt="..." style="width: 150px; height:150px;" />
+                      </div>
+                      <div class="m-2">
+                        <input
+                          type="file"
+                          @change="onFileSelected"
+                          class="bg-success col-10"
+                        />
+                      </div>
+                      <div class="m-2">
 
-            <div class="icon-user ">
-                <img
-                  class="userico "
-                  src="../../assets/userico.svg"
-                  alt=""
-                />
+                          <button @click="onUpload" class="col-5 mr-2 btn btn-success">CARGAR</button>
+                          <button @click="editarImg" class="col-5 ml-2 btn btn-success">SALIR</button>
+                      </div>
+
+                      
+                    </div>
+                    
+                  </div>
+                
+              
+
+              <div class="">
+                <p class="text-dark mt-3">{{ userUp.name }}</p>
+              </div>
+              <div class="">
+                <p class="text-dark">{{ userUp.email }}</p>
               </div>
 
-            <div class=" ">
+              <div class="">
+                <p class="text-dark">
+                <strong> Rol de usuario:</strong> {{ userUp.rol }}
+              </p>
+              </div>
+              <div class="">
+                <p class="text-dark">
+                <strong>Plan actual:</strong> {{ userUp.plan }}
+              </p>
+              </div>
+              <div class="">
+                <p class="text-dark text-center">
+                  Lima,Peru
+                  <img class="icoban" src="../../assets/logo-bandera.svg" />
+                </p>
+              </div>
+            </div>
+
+          </div>
+       
+
+          
+          <div class="row m-2 bg-white shadow bg-body rounded text-center w-100">
+
+            <div class="row d-flex flex-column justify-content-center align-items-center text-center my-3 " action="">
+
+              <p class="text-dark m-2">cambiar contraseña</p>
+            <div>
                 <input
-                  class="bg-light border border-white rounded text-center"
+                class="contraseña col-12 bg-light border border-secundary rounded m-2"
+                type="text"
+                required
+                placeholder="Contraseña"
+                 />
+            </div>
+              
+            <div>
+              <input
+                class="contraseña col-12 bg-light border border-secundary rounded m-2"
+                type="text"
+                required
+                placeholder="Repetir Contraseña"
+              />
+            </div>
+              <div class="mt-2">
+                <input
+                  class="btn-custom  "
+                  type="submit"
+                  value="Guardar"
+                  name=""
+                  id=""
+                />
+              </div>
+            </div>
+
+          </div>
+          
+
+      </div>
+
+
+      <div class="cuadro2 col-md-7 text-center ">
+
+        <div class="bg-white shadow bg-body rounded w-100 d-flex flex-column justify-content-center align-items-center m-2">
+            <div class="mb-3 mt-5">
+                
+                <input
+                  class="bg-light border border-secundary rounded text-center col-5 mr-2"
                   v-model="userUp.name"
                   type="text"
                   required
@@ -87,80 +122,87 @@
                 />
 
                 <input
-                  class="bg-light border border-white rounded text-center"
+                  class="bg-light border border-secundary rounded text-center col-5 ml-2"
                   v-model="userUp.last_name"
                   type="text"
                   required
                   placeholder="Apellidos"
                 />
-            </div>
-          </div>
+              </div>
 
-          <div class="region my-4">
-            <input
-              class=" bg-light border border-white rounded col-5 mr-2"
+              <div class="my-3">
+                <input
+                class="bg-light border border-secundary rounded col-5 mr-2"
+                type="text"
+                required
+                placeholder="Ciudad"
+                v-model="userUp.city"
+              />
+
+              <select
+                class="paisApi bg-light border border-secundary rounded  col-5 ml-2"
+                v-model="userUp.country"
+              >
+                <option value="" selected></option>
+                <option v-for="pais in namePais" :key="pais.common">
+                  {{ pais.common }}
+                </option>
+              </select>
+              </div>
+
+              <div class="my-3">
+                <input
+              class=" col-5 bg-light border border-secundary rounded mr-2"
               type="text"
               required
-              placeholder="Ciudad"
-              v-model="userUp.city"
+              placeholder="Genero"
+              disabled
             />
-            <!-- <select class="ciudadApi" > 
-                <option value="" selected> Ciudad</option>
-            </select>  -->
+            <input
+              class=" col-5 bg-light rounded border border-secundary ml-2"
+              v-model="userUp.date_birth"
+              type="date"
+              required
+              placeholder="Fecha de nacimiento"
+            />
+              </div>
 
-            <select
-              class="paisApi bg-light  rounded p-2 col-5 ml-2"
-              v-model="userUp.country"
-            >
-              <option value="" selected></option>
-              <option v-for="pais in namePais" :key="pais.common">
-                {{ pais.common }}
-              </option>
-            </select>
+              <div class="biografia my-3">
+               <textarea
+              class="bg-light border border-secundary rounded p-2"
+              v-model="userUp.biography"
+              name=""
+              id=""
+              required
+              placeholder="Biografia"
+              style="width: 350px; height: 150px;"
+            ></textarea>
+            </div>
+
+            <div class="botonguardar my-3">
+            <input
+              @click="userUpdate"
+              class="btn-custom "
+              type="submit"
+              value="Guardar"
+              name=""
+              id=""
+            />
           </div>
+
+        </div>
+
         
       </div>
 
-
-      <div class="data2 my-4">
-        <input
-          class="mx-2 col-4 bg-light border border-white p-2"
-          type="text"
-          required
-          placeholder="Genero"
-          disabled
-        />
-        <input
-          class="mx-2 col-6 bg-light border border-white rounded p-2"
-          v-model="userUp.date_birth"
-          type="date"
-          required
-          placeholder="Fecha de nacimiento"
-        />
-      </div>
-
-      <div class="biografia my-5">
-        <textarea
-          class="col-10 bg-light border border-white rounded"
-          v-model="userUp.biography"
-          name=""
-          id=""
-          required
-          placeholder="Biografia"
-          style="height: 100px"
-        ></textarea>
-      </div>
-      <div class="botonguardar mt-5">
-        <input
-          @click="userUpdate"
-          class="btn-custom col-4"
-          type="submit"
-          value="Guardar"
-          name=""
-          id=""
-        />
-      </div>
     </div>
+
+    <div v-if="subido" class="subiendo d-flex flex-column justify-content-center align-items-center">
+    <div class="spinner-grow" style="width: 10rem; height: 10rem;" role="status">
+     <span class="visually-hidden"></span>
+     </div> 
+    </div>
+    
   </div>
 </template>
 
@@ -172,10 +214,14 @@ export default {
 
   data() {
     return {
-      photo: "",
+      subido:null,
+      mostrar: null,
+      selectedFile: null,
+      picture: localStorage.getItem("photo_user"),
 
+      photo: "",
       userUp: {
-        email: "",
+        email: localStorage.getItem("email_user"),
         name: localStorage.getItem("name_user"),
         last_name: localStorage.getItem("last_name_user"),
         date_birth: localStorage.getItem("date_birth_user"),
@@ -183,17 +229,17 @@ export default {
         biography: localStorage.getItem("biography_user"),
         city: localStorage.getItem("city"),
         rol: "",
-        plan: ""
+        plan: "",
       },
 
       cuaDetalles: "",
 
       pais: [],
       namePais: [],
-      
     };
   },
   created() {
+    
     //this.userUp.last_name = localStorage.getItem("last_name_user");
     this.photo = localStorage.getItem("photo_user");
     //this.userUp.date_birth = localStorage.getItem("date_birth_user");
@@ -205,8 +251,7 @@ export default {
 
     //this.userUp.country = localStorage.getItem("country_user");
     this.obtenePaises();
-    this.userAccountType()
-  
+    this.userAccountType();
   },
   computed: {
     ...mapState("user", ["id_user"]),
@@ -216,7 +261,7 @@ export default {
       this.axios
         .post("/user/update", this.userUp)
         .then(() => {
-          // console.log(res);
+          //console.log("este"+res.data);
         })
         .catch(() => {
           //console.log(error);
@@ -247,185 +292,80 @@ export default {
       });
     },
 
-
-    userAccountType(){
+    userAccountType() {
       this.axios.get("dashboard/getattributes").then((r) => {
         const rs = r.data.data;
         this.userUp.plan = rs.accountType;
         this.userUp.rol = rs.role;
-      })
-    }
+      });
+    },
+
+    editarImg() {
+      this.mostrar = !this.mostrar;
+    },
+
+    onFileSelected(event) {
+      this.selectedFile = event.target.files[0];
+
+    },
+
+    onUpload() {
+      const fd = new FormData();
+      fd.append("user-photo", this.selectedFile, this.selectedFile.name);
+
+      this.subido = true;
+      this.axios
+        .post("/profile/upload-photo", fd)
+        .then(() => {
+          this.list();
+          
+        })
+        .catch(() => {});
+    },
+
+    list() {
+      this.axios.get(`/profile/info`).then((res) => {
+        localStorage.setItem("photo_user", res.data.photo);
+        console.log(res);
+        this.subido = false;
+        alert("SU NUEVA IMAGEN SE ACTUALIZO CON EXITO")
+        location. reload()
+      });
+
+      
+    },
   },
-  mounted() {},
 };
 </script>
 
 
 
 <style scope>
-.contenedor {
+.userico {
+  width: 35px;
+}
+.editImg {
+  z-index: 100;
+  position: absolute;
+  background: rgba(103, 99, 99, 0.676);
+  border-radius: 15px;
+  box-shadow: 1px 1px 2px 4px rgb(116, 177, 104);
+}
+.imagen {
+  border-radius:50%;
+}
+.img-edit{
+  border-radius:50%;
+}
+.subiendo{
+  background: rgba(110, 181, 108, 0.602);
+  position: absolute;
+  top:0x;
+  bottom:0;
+  right:0;
+  left:0;
+  z-index: 1000;
+  height:100%;
   width: 100%;
-  height: 100%;
-  padding: 20px 20px 20px 20px;
-  display: grid;
-  gap: 1rem;
-  grid-template-columns: repeat(3, 1fr);
-  grid-template-rows: repeat(3, minmax(min(100%, 15rem), 1fr));
 }
-
-.cuadro1 {
-  
-  grid-column: 1/2;
-  grid-row: 1/2;
-  display: grid;
-  grid-template-rows: (2, 1fr);
-}
-
-.cuadro2 {
-  grid-column: 1/2;
-  grid-row: 2/4;
-}
-
-.cuadro3 {
-  grid-column: 2/4;
-  grid-row: 1/4;
-}
-
-.parrafos {
-  overflow:hidden;
-  grid-row: 1/2;
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  grid-template-rows: repeat(2, 1fr);
-  
-  
-}
-
-.perfil {
-  grid-row: 2/3;
-  padding-left: 5% ;
-  margin-top: 5px;
-  margin-bottom: 5px;
-}
-
-.imagen1{
-  border-radius: 50%;
-  width: 40%;
-}
-.img1{
-  grid-column: 1/2;
-  grid-row: 1/3;
-  align-items: center;
-  display: flex;
-  justify-content: center;
-}
-
-
-.p1{
-  grid-column: 2/4;
-  grid-row: 1/2;
-  display: flex;
-  justify-content: flex-start;
-  margin-top: 30px;
-}
-.p2{
-  grid-column: 2/4;
-  grid-row: 2/3;
-  display: flex;
-}
-
-
-.cuadro2-1{
-display: grid;
-grid-template-columns: repeat(7, 1fr);
-grid-template-rows: repeat(2, 1fr);
-}
-.img2{
-  grid-column: 1/3;
-  grid-row: 1/3;
-  align-items: center;
-  display: flex;
-  justify-content: center;
-  flex-wrap: wrap;
-}
-.nombre{
-  grid-column: 3/8;
-  grid-row: 1/2;
-  display: flex;
-  margin-top: 15px;
-}
-.region{
-  grid-column: 3/8;
-  grid-row: 2/3;
-  display: flex;
-  align-items: center;
-  overflow: hidden;
-}
-
-.imagen2{
-  border-radius: 50%;
-  width: 50%;
-}
-
-
-
-@media only screen and (max-width: 1100px) {
-  .contenedor {
-    grid-template-rows: repeat(3, 1fr);
-    grid-template-columns: repeat(auto-fit, minmax(min(100%, 25rem), 1fr));
-  }
-
-  .cuadro1 {
-    grid-column: 1/2;
-    grid-row: 1/2;
-  }
-
-  .cuadro2 {
-    grid-column: 1/2;
-    grid-row: 2/3;
-  }
-
-  .cuadro3 {
-    grid-column: 1/2;
-    grid-row: 3/4;
-  }
-  
-}
-
-@media only screen and (max-width: 700px) {
-
-.cuadro2-1{
-  overflow:hidden;
-display: grid;
-grid-template-columns: repeat(5, 1fr);
-grid-template-rows: repeat(3, 1fr);
-text-align: center;
-vertical-align: middle;
-}
-.img2{
-  width: 30%;
-  grid-column: 1/6;
-  grid-row: 1/2;
-  margin: auto;
-  margin-top: 20px;
-}
-.nombre{
-  grid-column: 1/6;
-  grid-row: 2/3;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  margin-left: 20px;
-}
-.region{
-  grid-column: 1/6;
-  grid-row: 3/4;
-  display: flex;
-  justify-content: center;
-  margin: auto;
-}
-
-}
-
-
 </style>
