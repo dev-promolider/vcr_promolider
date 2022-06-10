55aef42c210d783fa005af894fb0e29b4f19500c
kenyichavez <chavezkenyi2@gmail.com>
2022-06-10 10:09:26 +0000

    vista de pago

---
+++ src/views/content/buy/buy.vue
@@ -0,0 +1,21 @@
+<template lang="">
+    <div class="buy">
+        <Buy></Buy>
+    </div>
+</template>
+<script>
+import Buy from '@/components/Buy/Buy.vue'
+export default {
+    name: 'buy',
+    components: {
+        Buy
+    }
+    
+}
+</script>
+<style scoped>
+    .buy{
+        width: 95%;
+        margin: 3%;
+    }
+</style>
