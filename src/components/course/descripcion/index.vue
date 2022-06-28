0db484daf33851b5a1acb4d617bde7409a1e60e9
Andrei <dyausinoh@gmail.com>
2022-06-28 11:46:15 -0500

    vcr-48

--- src/components/course/descripcion/index.vue
+++ src/components/course/descripcion/index.vue
@@ -75,6 +75,7 @@
                     
                       <v-card flat v-if="item.tab === 'Exámen'">
                    
+                          <div v-if="dataEx">
 
                             <div class="mt-4" v-if="dataEx.data === 'No existe el examen'">
                               <p>Esta lección no tiene ninguna prueba.</p><br><p>Continua la siguiente lección.</p>
@@ -84,6 +85,7 @@
                                 <button @click="Testing" class="test">Realizar prueba</button>
                               </p>
                             </div>
+                          </div>
                      
                       
                     </v-card> 
--- src/components/dashboard/index.vue
+++ src/components/dashboard/index.vue
@@ -17,7 +17,25 @@
           </div>
         </div>
       </div>
-      
+    <div class="share-network-list">
+
+       <ShareNetwork
+          v-for="network in networks"
+          :network="network.network"
+          :key="network.network"
+          :style="{backgroundColor: network.color}"
+          :url="sharing.url"
+          :title="sharing.title"
+          :description="sharing.description"
+          :quote="sharing.quote"
+          :hashtags="sharing.hashtags"
+          :twitterUser="sharing.twitterUser"
+        >
+          <i :class="network.icon"></i>
+          <span>{{ network.name }}</span>
+        </ShareNetwork>
+    </div>
+     
   </div>
 </template>
 <script>
@@ -34,7 +52,25 @@ export default {
     ClienteVenta,
   },
   data() {
-    return {};
+    return {
+      sharing: {
+        url: 'https://news.vuejs.org/issues/180',
+        title: 'Say hi to Vite! A brand new, extremely fast development setup for Vue.',
+        description: 'This week, I’d like to introduce you to "Vite", which means "Fast". It’s a brand new development setup created by Evan You.',
+        quote: 'The hot reload is so fast it\'s near instant. - Evan You',
+        hashtags: 'vuejs,vite,javascript',
+      },
+      
+      networks: [
+        { network: 'facebook', name: 'Facebook', icon: 'fab fah fa-lg fa-facebook-f', color: '#1877f2' },
+        { network: 'linkedin', name: 'LinkedIn', icon: 'fab fah fa-lg fa-linkedin', color: '#007bb5' },
+        { network: 'pinterest', name: 'Pinterest', icon: 'fab fah fa-lg fa-pinterest', color: '#bd081c' },
+        { network: 'skype', name: 'Skype', icon: 'fab fah fa-lg fa-skype', color: '#00aff0' },
+        { network: 'telegram', name: 'Telegram', icon: 'fab fah fa-lg fa-telegram-plane', color: '#0088cc' },
+        { network: 'twitter', name: 'Twitter', icon: 'fab fah fa-lg fa-twitter', color: '#1da1f2'},
+        { network: 'whatsapp', name: 'Whatsapp', icon: 'fab fah fa-lg fa-whatsapp', color: '#25d366'},
+      ]
+    };
   },
 };
 </script>
@@ -47,6 +83,38 @@ export default {
   .row-end{
     gap: 20px
   }
+}
+a{
+  flex: none;
+    color: #FFFFFF !important;
+    background-color: #333;
+    border-radius: 3px;
+    overflow: hidden;
+    display: flex;
+    flex-direction: row;
+    align-content: center;
+    align-items: center;
+    cursor: pointer;
+    margin: 0 10px 10px 0;
+    text-decoration: none !important;
+}
+.share-network-list {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: center;
+    max-width: 1000px;
+    margin: auto;
+}
+a[class^="share-network-"] .fah {
+    background-color: rgba(0, 0, 0, 0.2);
+    padding: 10px;
+    flex: 0 1 auto;
+}
+a[class^="share-network-"] span {
+    padding: 0 10px;
+    flex: 1 1;
+    font-weight: 500;
 }
 /* .container-fluid {
   overflow: auto !important;
