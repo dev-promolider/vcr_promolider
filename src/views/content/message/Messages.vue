ff1a61fbd1ba1b753bbd8b0eabda87d7985c5516
Diego Sanchez <diegopalominosa@gmail.com>
2022-07-10 17:25:19 -0500

    change messages flow

--- src/views/content/message/Messages.vue
+++ src/views/content/message/Messages.vue
@@ -1,21 +1,35 @@
 <template>
-        <Message/>
+  <Message :user="user"/>
 </template>
 
 <script>
-import Message from "@/components/message"
+import Message from "@/components/message";
 export default {
-    name: "Messages",
-    components: {
-        Message
-    },
-    mounted(){        
-    },
-    created(){        
-    }
-}
+  name: "Messages",
+  components: {
+    Message,
+  },
+
+  data() {
+    return {
+      user: {
+        id: localStorage.getItem("id_user"),
+        photo_user: localStorage.getItem("photo_user"),
+      },
+    };
+  },
+  mounted() {},
+  created() {},
+  methods: {
+    // async listPreferences() {
+    //   const resp = await this.axios.get("category/list");
+    //   if (resp.status == 200) {
+    //     const { data } = resp.data;
+    //     const preferences = this.setSelected(data);
+    //     this.preferences = preferences;
+    //     this.isLoading = true;
+    //   }
+    // },
+  },
+};
 </script>
-
-<style>
-
-</style>
--- src/views/content/optionpreferences/OptionPreferences.vue
+++ src/views/content/optionpreferences/OptionPreferences.vue
@@ -1,218 +1,217 @@
 <template>
-
-   <div class="section-option-preferences">
-            <div class="contenedor" >
-                    <div v-if="isLoading">
-                        <h4 class="title-preference ">Seleccione sus categorías  
-                             <i id="tooltip-target-1" class="fas fa-info-circle pointer"></i>
-                             <b-tooltip placement="right" target="tooltip-target-1" triggers="hover">
-                                 Obtenga recomendaciones personalizadas.</b-tooltip></h4>
-                        <div class="contenedor-preferences" >
-                            <div 
-                            class="preferences" 
-                            :class="[{ 'opacity': preference.selected }]" 
-                            v-for="preference in preferences" 
-                            :key="preference.id" 
-                            @click="editPreference( preference )">
-                                <i :class="preference.icon "></i>
-                                <div class="name-preference">{{preference.name}}</div>
-                            </div>
-                        </div>
-                        <div class="button-preferences">
-                            <button class="btn button-add" @click="addPreferences">Guardar</button>
-                        </div>
-                    </div>
-                    <b-spinner class="loading" label="Spinning" type="grow"  v-else></b-spinner>
-            </div>
-   </div>
+  <div class="section-option-preferences">
+    <div class="contenedor">
+      <div v-if="isLoading">
+        <h4 class="title-preference">
+          Seleccione sus categorías
+          <i id="tooltip-target-1" class="fas fa-info-circle pointer"></i>
+          <b-tooltip
+            placement="right"
+            target="tooltip-target-1"
+            triggers="hover"
+          >
+            Obtenga recomendaciones personalizadas.</b-tooltip
+          >
+        </h4>
+        <div class="contenedor-preferences">
+          <div
+            class="preferences"
+            :class="[{ opacity: preference.selected }]"
+            v-for="preference in preferences"
+            :key="preference.id"
+            @click="editPreference(preference)"
+          >
+            <i :class="preference.icon"></i>
+            <div class="name-preference">{{ preference.name }}</div>
+          </div>
+        </div>
+        <div class="button-preferences">
+          <button class="btn button-add" @click="addPreferences">
+            Guardar
+          </button>
+        </div>
+      </div>
+      <b-spinner
+        class="loading"
+        label="Spinning"
+        type="grow"
+        v-else
+      ></b-spinner>
+    </div>
+  </div>
 </template>
 
 <script>
-import Vue from 'vue'
+import Vue from "vue";
 export default {
-    data(){
-        return{
-            preferences: [],
-            list: [],
-            formData: [],
-            isLoading: false
+  data() {
+    return {
+      preferences: [],
+      list: [],
+      formData: [],
+      isLoading: false,
+    };
+  },
+  updated() {},
+  created() {
+    this.setPreferencesList();
+    this.listPreferences();
+  },
+  methods: {
+    async listPreferences() {
+      const resp = await this.axios.get("category/list");
+
+      if (resp.status == 200) {
+        const { data } = resp.data;
+        const preferences = this.setSelected(data);
+        this.preferences = preferences;
+        this.isLoading = true;
+      }
+    },
+    editPreference(preferences) {
+      preferences.selected = !preferences.selected;
+      if (!this.list.includes(preferences.id)) {
+        this.list.push(preferences.id);
+      } else {
+        const catg = this.list.indexOf(preferences.id);
+        this.list.splice(catg, 1);
+      }
+    },
+    async addPreferences() {
+      if (this.list.length >= 3) {
+        let preferences = { categorys: this.list };
+        const resp = await this.axios.post("/preferences/add", preferences);
+        if (resp.status === 200) {
+          this.makeToast("success", "Categoria Guardada");
+        } else {
+          this.makeToast("danger", "Error al guardar");
         }
+      } else {
+        this.makeToast("danger", "Mínimo 3 categorias");
+      }
     },
-    updated(){
-      
+    setSelected(data) {
+      data.forEach((preference) => {
+        Vue.set(preference, "selected", true);
+
+        this.list.forEach((list) => {
+          if (list === preference.id) {
+            Vue.set(preference, "selected", false);
+          }
+        });
+      });
+
+      return data;
     },
-    created(){
-        this.setPreferencesList()
-        this.listPreferences()
+    setPreferencesList() {
+      this.list = [];
     },
-    methods:{
-        async listPreferences(){
-            const  resp = await this.axios.get('category/list')
-
-            if(resp.status == 200){
-                const { data } = resp.data 
-                const preferences =  this.setSelected( data )
-                this.preferences = preferences
-                this.isLoading = true
-            }
-
-
-        },
-        editPreference( preferences ){
-            preferences.selected = !preferences.selected
-            if (!this.list.includes(preferences.id)) {
-                this.list.push(preferences.id);
-            } else {
-                const catg = this.list.indexOf(preferences.id);
-                this.list.splice(catg, 1);
-            }
-        },
-        async addPreferences(){
-            if(this.list.length >= 3){
-                let preferences = {categorys: this.list,}
-                const resp = await this.axios.post('/preferences/add', preferences)
-                if( resp.status === 200){
-                    this.makeToast( 'success', 'Categoria Guardada' )
-                }else{
-                    this.makeToast( 'danger', 'Error al guardar' )
-                }
-
-            }else{
-                this.makeToast( 'danger' , 'Mínimo 3 categorias')
-            }
-        },
-        setSelected( data ){
-           
-            data.forEach((preference) => {
-
-                Vue.set(preference, 'selected', true );
-
-                    this.list.forEach(( list ) => {
-                        
-                        if( list === preference.id ){
-                            Vue.set(preference, 'selected', false );
-                        }
-
-
-                    })
-
-            })
-             
-            return data
-        },
-        setPreferencesList(){
-           this.list = []
-        },
-        makeToast( variant, msg ) {
-                this.$bvToast.toast( msg , {
-                title: 'Categorias',
-                variant: variant,
-                toaster: 'b-toaster-top-center',
-                solid: true
-            })
-        }
-
+    makeToast(variant, msg) {
+      this.$bvToast.toast(msg, {
+        title: "Categorias",
+        variant: variant,
+        toaster: "b-toaster-top-center",
+        solid: true,
+      });
     },
-
-}
+  },
+};
 </script>
 
 <style scoped>
-    .pointer{
-        cursor: pointer;
-    }
-    .btn{
-        border-radius: 2rem;
-    }
-    .btn-light{
-        background-color: #131b1e;
-        color: white;
-    }
-    .btn-light:hover{
-        background-color: #131b1e;
-         color: white;
-    }
-    .title-preference{
-        padding: 20px 0 ;
-        font-weight: bold;
-    }
-    .loading{
-        margin-top: 200px;
-        font-size: 50px;
-        width: 80px;
-        height: 80px;
-    }
-    .contenedor{
-        width: 100%;
-        max-width: 1200px;
-        text-align: center;
-        margin: 0 auto;
-    }
-    .contenedor-preferences{
-        display: flex;
-        flex-wrap: wrap;
-        align-items: center;
-        justify-content: center;
-        overflow: scroll;
-    }
-   
-  .preferences{
-      padding: 0 auto;
-      margin: 15px;
-      width: 120px;
-      text-align: center;
-      color: #5dd04e ;
-      cursor: pointer;
-  }
-  .preferences:hover{
-      animation: temblar 3s 3.5 alternate;
+.pointer {
+  cursor: pointer;
+}
+.btn {
+  border-radius: 2rem;
+}
+.btn-light {
+  background-color: #131b1e;
+  color: white;
+}
+.btn-light:hover {
+  background-color: #131b1e;
+  color: white;
+}
+.title-preference {
+  padding: 20px 0;
+  font-weight: bold;
+}
+.loading {
+  margin-top: 200px;
+  font-size: 50px;
+  width: 80px;
+  height: 80px;
+}
+.contenedor {
+  width: 100%;
+  max-width: 1200px;
+  text-align: center;
+  margin: 0 auto;
+}
+.contenedor-preferences {
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+  overflow: scroll;
+}
 
+.preferences {
+  padding: 0 auto;
+  margin: 15px;
+  width: 120px;
+  text-align: center;
+  color: #5dd04e;
+  cursor: pointer;
+}
+.preferences:hover {
+  animation: temblar 3s 3.5 alternate;
+}
+.button-add {
+  background: #131b1e;
+  margin: 1rem;
+  width: 150px;
+  color: #efefef;
+}
+.button-add:hover {
+  background: #192327;
+  color: #efefef;
+}
+.button-preferences {
+  text-align: end;
+}
+.opacity {
+  opacity: 0.25;
+  color: black;
+}
+.preferences i {
+  font-size: 55px;
+}
+.name-preference {
+  font-size: 20px;
+}
+@media (max-width: 768px) {
+  .preferences i {
+    font-size: 30px;
   }
-  .button-add{
-      background: #131b1e;
-      margin: 1rem ;
-      width: 150px;
-      color: #efefef;
+  .contenedor {
+    width: 95%;
   }
-  .button-add:hover{
-      background: #192327;
-      color: #efefef;
+}
+@keyframes temblar {
+  0% {
+    transform: rotate(-15deg);
   }
-  .button-preferences{
-      text-align: end;
+  50% {
+    transform: rotate(0deg);
   }
-  .opacity{
-      opacity: 0.25;
-      color: black
+  100% {
+    transform: rotate(15deg);
   }
-  .preferences i{
-      font-size: 55px;
-  }
-  .name-preference{
-      font-size: 20px;
-  }
-  @media (max-width: 768px) {
-      .preferences i{
-          font-size: 30px;
-      }
-      .contenedor{
-          width: 95%;
-      }
-      
-  }
-    @keyframes temblar {
-    0%{
-        transform: rotate(-15deg);
-    }
-    50% {
-        transform: rotate(0deg);
-    }
-    100%{
-        transform: rotate(15deg);
-    }
-    }
-   .tooltip{
-       position: relative;
-       height: 100px;
-   }
+}
+.tooltip {
+  position: relative;
+  height: 100px;
+}
 </style>
