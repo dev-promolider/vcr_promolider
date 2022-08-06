a88f12deb587389235e01d450da10e6b43ac18ef
Diego Sanchez <diegopalominosa@gmail.com>
2022-08-06 11:38:36 -0500

    change ui course

--- src/components/course/comentarios/valoraciones.vue
+++ src/components/course/comentarios/valoraciones.vue
@@ -1,162 +1,183 @@
 <template>
-  <div>
-
-      <v-card dense style="border-radius: 15px" :key="cardRender">
-
-
-      
-            <v-list-item class="pt-1">
-              
-              <v-list-item-content>
-              
-                <v-list-item-title class="font-weight-bold">
-                  <h4 class="display-6">Comentarios</h4>
-                </v-list-item-title>
-
-                <v-list-item-title class="font-weight-bold">
-                  <h4 class="display-6" v-if="avg != 0">Puntaje promedio : {{avg}}</h4>
-                  <div v-else>
-                  <h4 class="display-6">!Sé el primero en comentar y valorar el curso!</h4>
-                  </div>
-                </v-list-item-title>
-
-                <v-list-item-subtitle>
-                  <v-rating
-                    color="warning"
-                    hover
-                    readonly
-                    length="5"
-                    size="40"
-                    :value ="avg"
-                    half-increments
-                  ></v-rating>
-                </v-list-item-subtitle>
-
-                <v-list-item-subtitle class="font-weight-bold">
-                  <span>Valoracion del Curso </span>
-                </v-list-item-subtitle>
-                
-              </v-list-item-content>
-            
-            </v-list-item>
-    
-
-
-        <v-list-item three-line style="width: 100%; padding-top: 20px">
-          <v-list-item-avatar>
-            <v-img :src="img"></v-img>
-          </v-list-item-avatar>
-
-          <v-list-item-content>
-            <v-form @submit.prevent="sendRating()" ref="form" lazy-validation>
-              <v-text-field
-                label="Escribe un comentario..."
-                v-model="newRating.commentary"
-                clearable
-                outlined
-                required
-                color="black"
-              ></v-text-field>
-              <v-rating
-                    color="warning"
-                    hover
-                    length="5"
-                    size="30"
-                    v-model="newRating.rate"
-                    half-increments
-                  ></v-rating>
-
-            </v-form>
-             <v-alert
-             v-model="alert"
-             dismissible
-             elevation="11"
-             type="warning"
-             >{{ alertMessage }}</v-alert>
-             <v-alert
-             v-model="alert2"
-             dismissible
-             elevation="11"
-             type="success"
-             >{{ alertMessage }}</v-alert>    
-          </v-list-item-content>
-        </v-list-item>
-        <div
-          class="d-flex justify-content-center align-items-center py-5"
-          v-if="isLoadingRating"
+  <div :key="cardRender">
+    <div class="row">
+      <div class="col-md-12">
+        <p
+          class="text-left text-white p-3"
+          style="font-size: 1.3em; padding-bottom: 0 px"
         >
-          <v-progress-circular indeterminate color="grey"></v-progress-circular>
+          Comentarios
+        </p>
+        <div class="ml-5">
+          <img :src="S3_PATH + allRating[0].url_portada" width="200" />
         </div>
-        <div
-          v-if="isLoadingRating === false && allRating.length === 0"
-          class="no-result center-element py-5"
+      </div>
+
+      <div class="col-md-12 text-white">
+        <p
+          class="text-left text-white"
+          style="font-size: 1.3em; display: inline; padding-left: 20px"
         >
-          <span>Aún no hay Valoraciones del Curso</span>
-        </div>
+          {{ avg }}
+        </p>
+        <v-rating
+          style="display: inline"
+          color="warning"
+          hover
+          readonly
+          length="5"
+          size="20"
+          :value="avg"
+          half-increments
+        ></v-rating>
+      </div>
+    </div>
 
-        <v-list
-          class="content-comments"
-          v-if="isLoadingRating === false && allRating.length > 0"
-          three-line
-        >
-          <template v-for="(i, index) in comments">
-            <v-list-item class="pt-1" :key="index">
-              <v-list-item-avatar>
-                <v-img :src="i.photo"></v-img>
-              </v-list-item-avatar>
+    <v-list-item class="pt-1">
+      <v-list-item-content>
+        <v-list-item-title class="font-weight-bold">
+          <h4 class="display-6">Comentarios</h4>
+        </v-list-item-title>
 
-              <v-list-item-content>
-                <v-list-item-title class="font-weight-bold">
-                  {{ i.name }}
-                </v-list-item-title>
+        <v-list-item-title class="font-weight-bold">
+          <h4 class="display-6" v-if="avg != 0">
+            Puntaje promedio : {{ avg }}
+          </h4>
+          <div v-else>
+            <h4 class="display-6">
+              !Sé el primero en comentar y valorar el curso!
+            </h4>
+          </div>
+        </v-list-item-title>
 
-                <v-list-item-subtitle>
-                  <v-rating
-                    color="warning"
-                    hover
-                    readonly
-                    length="5"
-                    size="30"
-                    :value ="i.rate"
-                    half-increments
-                  ></v-rating>
-                </v-list-item-subtitle>
+        <v-list-item-subtitle>
+          <v-rating
+            color="warning"
+            hover
+            readonly
+            length="5"
+            size="40"
+            :value="avg"
+            half-increments
+          ></v-rating>
+        </v-list-item-subtitle>
 
-                <v-list-item-subtitle>
-                  {{ date(i.created_at) }}
-                </v-list-item-subtitle>
-                <v-list-item-subtitle class="text--primary">
-                  <v-card-text style="padding: 0">
-                    <vue-show-more-text
-                      :text="i.commentary"
-                      :lines="2"
-                      more-text="Ver más.."
-                      less-text="Ocultar"
-                      additional-container-css="padding: 8px 0  0 0; width: 100% !important ; text-align: justify;"
-                      additional-anchor-css="color: green; text-decoration: none; width: 80px; margin:  0  0 0 auto; padding: 0"
-                    />
-                  </v-card-text>
-                  <v-divider class="py-0"></v-divider>
-                </v-list-item-subtitle>
-              </v-list-item-content>
-            </v-list-item>
-          </template>
-        </v-list>
-      </v-card>
-  </div>  
+        <v-list-item-subtitle class="font-weight-bold">
+          <span>Valoracion del Curso </span>
+        </v-list-item-subtitle>
+      </v-list-item-content>
+    </v-list-item>
+
+    <!-- <v-list-item three-line style="width: 100%; padding-top: 20px">
+      <v-list-item-avatar>
+        <v-img :src="img"></v-img>
+      </v-list-item-avatar>
+
+      <v-list-item-content>
+        <v-form @submit.prevent="sendRating()" ref="form" lazy-validation>
+          <v-text-field
+            label="Escribe un comentario..."
+            v-model="newRating.commentary"
+            clearable
+            outlined
+            required
+            color="black"
+          ></v-text-field>
+          <v-rating
+            color="warning"
+            hover
+            length="5"
+            size="30"
+            v-model="newRating.rate"
+            half-increments
+          ></v-rating>
+        </v-form>
+        <v-alert v-model="alert" dismissible elevation="11" type="warning">{{
+          alertMessage
+        }}</v-alert>
+        <v-alert v-model="alert2" dismissible elevation="11" type="success">{{
+          alertMessage
+        }}</v-alert>
+      </v-list-item-content>
+    </v-list-item> -->
+
+    <!-- <div
+      class="d-flex justify-content-center align-items-center py-5"
+      v-if="isLoadingRating"
+    >
+      <v-progress-circular indeterminate color="grey"></v-progress-circular>
+    </div> -->
+
+    <!-- <div
+        v-if="isLoadingRating === false && allRating.length === 0"
+        class="no-result center-element py-5"
+      >
+        <span>Aún no hay Valoraciones del Curso</span>
+      </div> -->
+
+    <!-- <v-list
+        class="content-comments"
+        v-if="isLoadingRating === false && allRating.length > 0"
+        three-line
+      >
+        <template v-for="(i, index) in comments">
+          <v-list-item class="pt-1" :key="index">
+            <v-list-item-avatar>
+              <v-img :src="i.photo"></v-img>
+            </v-list-item-avatar>
+
+            <v-list-item-content>
+              <v-list-item-title class="font-weight-bold">
+                {{ i.name }}
+              </v-list-item-title>
+
+              <v-list-item-subtitle>
+                <v-rating
+                  color="warning"
+                  hover
+                  readonly
+                  length="5"
+                  size="30"
+                  :value="i.rate"
+                  half-increments
+                ></v-rating>
+              </v-list-item-subtitle>
+
+              <v-list-item-subtitle>
+                {{ date(i.created_at) }}
+              </v-list-item-subtitle>
+              <v-list-item-subtitle class="text--primary">
+                <v-card-text style="padding: 0">
+                  <vue-show-more-text
+                    :text="i.commentary"
+                    :lines="2"
+                    more-text="Ver más.."
+                    less-text="Ocultar"
+                    additional-container-css="padding: 8px 0  0 0; width: 100% !important ; text-align: justify;"
+                    additional-anchor-css="color: green; text-decoration: none; width: 80px; margin:  0  0 0 auto; padding: 0"
+                  />
+                </v-card-text>
+                <v-divider class="py-0"></v-divider>
+              </v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+        </template>
+      </v-list> -->
+  </div>
 </template>
 
 <script>
 import { mapActions, mapGetters, mapState } from "vuex";
-import vueShowMoreText from "vue-show-more-text";
+// import vueShowMoreText from "vue-show-more-text";
 import moment from "moment";
 export default {
   name: "Comentarios",
   components: {
-    vueShowMoreText
+    // vueShowMoreText,
   },
   data() {
     return {
+      S3_PATH: "https://crm-storage-user.s3.eu-west-1.amazonaws.com/",
       alert: false,
       alert2: false,
       alertMessage: "",
@@ -185,72 +206,66 @@ export default {
       return this.getRating;
     },
     avg() {
-
-      if(this.getRating === undefined){
+      if (this.getRating === undefined) {
         return 0;
       }
 
-      if(this.getRating.length === 0){
+      if (this.getRating.length === 0) {
         return 0;
       }
 
-      var finalArray = this.getRating.map(function (obj) {
-      return obj.rate;
+      let finalArray = this.getRating.map(function (obj) {
+        return obj.rate;
       });
 
-      const average = arr => arr.reduce((a,b) => a + b, 0) / arr.length;
+      const average = (arr) => arr.reduce((a, b) => a + b, 0) / arr.length;
 
-      return average(finalArray); 
+      return average(finalArray);
     },
   },
 
-
-
   methods: {
     ...mapActions("course", ["setRating"]),
-    ...mapActions("course", { getRating2: "getRating",}),
+    ...mapActions("course", { getRating2: "getRating" }),
     // Funcion para el envio de mensajes
 
-
     async sendRating() {
-      
-        if(this.validationRating()){
+      if (this.validationRating()) {
+        this.newRating.course_id = this.$route.query.course;
 
-          this.newRating.course_id = this.$route.query.course;
-          
-          if (this.newRating.course_id != undefined) {
-
+        if (this.newRating.course_id != undefined) {
           await this.setRating(this.newRating);
           await this.getRating2(this.$route.query.course);
-          this.alertMessage="Gracias por valorar este curso!";
-          this.alert2=true;
+          this.alertMessage = "Gracias por valorar este curso!";
+          this.alert2 = true;
           this.newRating.commentary = "";
           this.newRating.rate = 0;
-          }}},
-
+        }
+      }
+    },
 
     validationRating() {
       if (this.newRating.commentary === "" && this.newRating.rate === 0) {
-        this.alertMessage="Escriba un comentario y deje una valoración";
-        this.alert=true;
+        this.alertMessage = "Escriba un comentario y deje una valoración";
+        this.alert = true;
         return false;
       }
 
       if (this.newRating.commentary === "") {
-        this.alertMessage="Comentario vacío! Escriba un comentario";
-        this.alert=true;
+        this.alertMessage = "Comentario vacío! Escriba un comentario";
+        this.alert = true;
         return false;
       }
 
       if (this.newRating.commentary === 0) {
-        this.alertMessage="Valoracion 0 estrellas! Debe ser mayor a 0";
-        this.alert=true;
+        this.alertMessage = "Valoracion 0 estrellas! Debe ser mayor a 0";
+        this.alert = true;
         return false;
       }
-    return true;
+      return true;
     },
     forceRerender() {
-      this.cardRender = this.cardRender +1;  
+      this.cardRender = this.cardRender + 1;
     },
     date(dt) {
       return moment(dt).format("DD-MM-YYYY");
--- src/components/course/descripcion/index.vue
+++ src/components/course/descripcion/index.vue
@@ -20,83 +20,109 @@
 
         <v-tabs-items v-model="tab">
           <v-tab-item v-for="item in items" :key="item.tab">
-            <v-card flat v-if="item.tab === 'Resumen'">
-              <v-card-text class="h6 text-justify">
+            <!-- Resumen -->
+            <v-card
+              flat
+              v-if="item.tab === 'Resumen'"
+              color="#131b1e"
+              rounded="xl"
+            >
+              <v-card-text class="h6 text-justify text-white">
                 {{ lesson.description }}
               </v-card-text>
             </v-card>
-
-            <v-card flat v-if="item.tab === 'Recursos'">
+            <!-- Recursos -->
+            <v-card
+              flat
+              v-if="item.tab === 'Recursos'"
+              color="#131b1e"
+              outlined
+              rounded="xl"
+              tag="div"
+            >
               <template v-if="!isResources">
-                <v-card-text class="h6 text-center">
+                <v-card-text class="h6 text-center text-white">
                   Ningún recurso disponible
                 </v-card-text>
               </template>
               <div v-else>
-                <ul class="list-group list-group-flush">
-                  <li
-                    class="list-group-item"
-                    v-for="(resource, index) in resources"
-                    :key="index"
-                  >
-                    <div class="d-flex justify-content-between">
-                      <div>
-                        {{ index + 1 }})
-                        <a
-                          style="font-size: 0.8em; text-decoration: none"
-                          disabled
-                        >
-                          <i class="fas fa-download"></i>
-                          {{ getNameResource(resource.resource_file) }}
-                        </a>
-                      </div>
-                      <div>
-                        <a href="#modal" class="open"
-                          ><button
-                            class="btn btn-primary btn-sm"
-                            @click="preView(resource)"
+                <v-card-text class="h6">
+                  <ul class="list-group list-group-flush">
+                    <li
+                      class="list-group-item"
+                      v-for="(resource, index) in resources"
+                      :key="index"
+                      style="background-color: #131b1e"
+                    >
+                      <div class="d-flex justify-content-between text-white">
+                        <div>
+                          {{ index + 1 }})
+                          <a
+                            style="font-size: 0.8em; text-decoration: none"
+                            disabled
                           >
-                            Ver Archivo
-                          </button></a
-                        >
+                            <i class="fas fa-download"></i>
+                            {{ getNameResource(resource.resource_file) }}
+                          </a>
+                        </div>
+                        <div>
+                          <a href="#modal" class="open">
+                            <button
+                              class="btn btn-primary btn-sm"
+                              @click="preView(resource)"
+                            >
+                              Ver Archivo
+                            </button></a
+                          >
+                        </div>
                       </div>
-                    </div>
-                  </li>
+                    </li>
 
-                  <div class="modal" id="modal">
-                    <a href="#" class="modal-bg"></a>
-                    <div class="modal-content">
-                      <a href="#" class="modal-exit">x</a>
-                      <div class="row m-5 container1 d-flex flex-column">
-                        <iframe :src="picture" class="pdf"> </iframe>
+                    <div class="modal" id="modal">
+                      <a href="#" class="modal-bg"></a>
+                      <div class="modal-content">
+                        <a href="#" class="modal-exit">x</a>
+                        <div class="row m-5 container1 d-flex flex-column">
+                          <iframe :src="picture" class="pdf"> </iframe>
 
-                        <br />
+                          <br />
 
-                        <button class="btn btn-primary" id="button">
-                          DESCARGAR
-                        </button>
+                          <button class="btn btn-primary" id="button">
+                            DESCARGAR
+                          </button>
 
-                        <div v-if="carga" class="cargando">
-                          <div class="spinner-border"></div>
+                          <div v-if="carga" class="cargando">
+                            <div class="spinner-border"></div>
+                          </div>
                         </div>
                       </div>
                     </div>
-                  </div>
-                </ul>
+                  </ul>
+                </v-card-text>
               </div>
             </v-card>
-
-            <v-card flat v-if="item.tab === 'Exámen'">
+            <!-- Examen -->
+            <v-card
+              flat
+              v-if="item.tab === 'Examen'"
+              color="#131b1e"
+              outlined
+              rounded="xl"
+              tag="div"
+            >
               <div v-if="dataEx">
-                <div
-                  class="mt-4 text-center"
+                <template
                   v-if="
                     dataEx.data === 'No existe el examen' ||
-                    dataEx.data === 'limite de intentos alcanzado'
+                    dataEx.data === 'limite de intentos alcanzado' ||
+                    dataEx.data === 'usted aprovo el examen'
                   "
                 >
-                  Ningún examen disponible
-                </div>
+                  <v-card-text class="h6 text-center text-white">
+                    Ningún examen disponible
+                  </v-card-text>
+                </template>
+
                 <div class="mx-4 mt-4" v-else>
                   <p class="text-justify">
                     <button @click="Testing" class="test">
@@ -107,7 +133,15 @@
               </div>
             </v-card>
 
-            <v-card flat v-if="item.tab === 'Dinámicas'" class="p-3">
+            <!-- Dinámicas -->
+            <v-card
+              flat
+              v-if="item.tab === 'Dinámicas'"
+              color="#131b1e"
+              outlined
+              rounded="xl"
+              tag="div"
+            >
               <template v-if="!isLoadingDinamic">
                 <div class="text-center">
                   <v-progress-circular indeterminate color="success">
@@ -130,14 +164,22 @@
                   </v-btn>
                 </template>
               </template>
+
               <template v-if="!stateDinamic && isLoadingDinamic">
-                <v-card-text class="text-center h6">
+                <v-card-text class="text-center h6 text-white">
                   Ninguna dinámica disponible
                 </v-card-text>
               </template>
             </v-card>
 
-            <v-card flat v-if="item.tab === 'Valoraciones'">
+            <v-card
+              flat
+              v-if="item.tab === 'Valoraciones'"
+              color="#131b1e"
+              outlined
+              rounded="xl"
+              tag="div"
+            >
               <Valoraciones />
             </v-card>
           </v-tab-item>
@@ -168,7 +210,7 @@ export default {
       items: [
         { tab: "Resumen" },
         { tab: "Recursos" },
-        { tab: "Exámen" },
+        { tab: "Examen" },
         { tab: "Dinámicas" },
         { tab: "Valoraciones" },
       ],
@@ -303,6 +345,11 @@ export default {
   margin-left: 7px;
 }
 
+/* .v-card > :last-child:not(.v-btn):not(.v-chip):not(.v-avatar) { */
+/* .v-card > :last-child {
+  background-color: #131b1e;
+  border-radius: 20px;
+} */
 /* .tab-pane {
   width: 100%;
   height: 215px;
@@ -324,31 +371,22 @@ export default {
   border-bottom: solid #e5e5e5 0.2px !important;
 }
 .nav-tabs li {
-  /* Makes a horizontal row */
   float: left;
-  /* So the psueudo elements can be
-     abs. positioned inside */
   position: relative;
   cursor: pointer;
 }
 .nav-tabs a {
-  /* Make them block level
-     and only as wide as they need */
   float: left;
   text-decoration: none;
-
-  /* Default colors */
   color: white;
   background: #c4c4c4;
   font-size: 18px;
 }
 .nav-tabs .active {
-  /* Highest, active tab is on top */
   z-index: 3;
   pointer-events: none;
 }
 .nav-tabs .active a {
-  /* Colors when tab is active */
   background: white;
   color: black;
 }
@@ -356,13 +394,10 @@ export default {
 .nav-tabs .nav-item:after,
 .nav-tabs .nav-link:before,
 .nav-tabs .nav-link:after {
-  /* All pseudo elements are 
-     abs. positioned and on bottom */
   position: absolute;
   bottom: 0;
 }
-/* Only the first, last, and active
-   tabs need pseudo elements at all */
+
 .nav-tabs .nav-item:after,
 .nav-tabs .nav-item a:after,
 .nav-tabs .nav-item:before,
@@ -376,11 +411,8 @@ export default {
 .nav-tabs .active:before,
 .nav-tabs .active:after {
   background: white;
-
-  /* Squares below circles */
   z-index: 1;
 }
-/* Squares */
 .nav-tabs li:before,
 .nav-tabs li:after {
   background: #c4c4c4;
