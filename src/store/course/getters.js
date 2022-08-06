3ab4bf3a3c19fb6757d3400c21d7fc660beafe39
Diego Sanchez <diegopalominosa@gmail.com>
2022-08-06 14:16:22 -0500

    change ui valoraciones

--- src/store/course/getters.js
+++ src/store/course/getters.js
@@ -5,16 +5,16 @@ export const isLoading = (state) => state.isLoading;
 export const resources = (state) => state.resources;
 export const isResources = (state) => state.isResources;
 export const completedLessons = (state) => state.completedLessons;
-export const urlVideo = (state) => state.urlVideo; 
+export const urlVideo = (state) => state.urlVideo;
 export const timeReady = (state) => state.timeReady;
 export const getProductor = (state) => state.productor;
 export const getComments = (state) => state.allComments;
-export const getRating = (state) => state.allRating;
+export const getRating = (state) => state.allRating.rates;
 export const getCourseRating = (state) => state.courseRating;
 export const getCourseActive = (state) => state.course_active;
 export const getDataEx = (state) => state.dataEx;
-export const getListLeaderBoard = ( state ) => state.leaderBoard.slice( 3 , state.leaderBoard.length )
-export const getFirstPlaceLeaderBoard = ( state ) => state.leaderBoard.slice(0,1)[0] || { name: 'Juan' , photo: require('@/assets/perfil-del-usuario.png') }
-export const getSecondPlaceLeaderBoard = ( state ) => state.leaderBoard.slice(1,2)[0] || { name: 'Roberto' , photo: require('@/assets/perfil-del-usuario.png') }
-export const getThirdPlaceLeaderBoard = ( state ) => state.leaderBoard.slice(2,3)[0] || { name: 'Lucia' , photo: require('@/assets/perfil-del-usuario.png') }
-    
+export const getListLeaderBoard = (state) => state.leaderBoard.slice(3, state.leaderBoard.length)
+export const getFirstPlaceLeaderBoard = (state) => state.leaderBoard.slice(0, 1)[0] || { name: 'Juan', photo: require('@/assets/perfil-del-usuario.png') }
+export const getSecondPlaceLeaderBoard = (state) => state.leaderBoard.slice(1, 2)[0] || { name: 'Roberto', photo: require('@/assets/perfil-del-usuario.png') }
+export const getThirdPlaceLeaderBoard = (state) => state.leaderBoard.slice(2, 3)[0] || { name: 'Lucia', photo: require('@/assets/perfil-del-usuario.png') }
+
